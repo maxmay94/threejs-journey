9f24b2b4c9fbf5bf254f2eca4e7047bf0a4f0fac
maxmay94 <maxmay94@gmail.com>
2023-01-26 10:50:37 -0500

    add triangle geometry

--- basics/09-geometries/src/script.js
+++ basics/09-geometries/src/script.js
@@ -11,8 +11,23 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 16, 16, 16)
+const positionsArray = new Float32Array([
+    0, 0, 0,
+    0, 1, 0,
+    1, 0, 0
+])
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position', positionsAttribute)
+
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000, 
+    wireframe: true 
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
