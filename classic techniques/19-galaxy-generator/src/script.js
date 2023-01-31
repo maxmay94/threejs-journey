991f946b0d805b23679ee3a7ebf4ea40c64e11ef
maxmay94 <maxmay94@gmail.com>
2023-01-31 14:32:36 -0500

    finish lesson 19

--- classic techniques/19-galaxy-generator/src/script.js
+++ classic techniques/19-galaxy-generator/src/script.js
@@ -43,7 +43,7 @@ const generateGalaxy = () => {
     }
     
     geometry = new THREE.BufferGeometry()
-    
+
     const positions = new Float32Array(parameters.count * 3)
     const colors = new Float32Array(parameters.count * 3)
 
@@ -67,9 +67,12 @@ const generateGalaxy = () => {
         positions[i3 + 2] = (Math.sin(branchAngle + spinAngle) * radius) + randomZ
 
         // Color
-        colors[i3 + 0] = 1
-        colors[i3 + 1] = 0
-        colors[i3 + 2] = 0
+        const mixedColor = colorInside.clone()
+        mixedColor.lerp(colorOutside, (radius / parameters.radius))
+
+        colors[i3 + 0] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
     }
     
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
