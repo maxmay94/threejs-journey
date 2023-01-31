16bbd5c827bd684bd9b60d8c9022e39e373ec149
maxmay94 <maxmay94@gmail.com>
2023-01-31 11:33:09 -0500

    add graves

--- classic techniques/17-haunted-house/src/script.js
+++ classic techniques/17-haunted-house/src/script.js
@@ -90,16 +90,34 @@ scene.add(graves)
 const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
 const graveMaterial = new THREE.MeshStandardMaterial({color: '#b2b6b1'})
 
+for(let i = 0; i < 50 ; i++) {
+    const angle = Math.random() * (Math.PI * 2)
+    const radius = 3 + Math.random() * 6
+    const x = Math.sin(angle) * radius
+    const z = Math.cos(angle) * radius
+
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    grave.position.x = x
+    grave.position.z = z
+    grave.position.y = 0.3
+
+    grave.rotation.y = (Math.random() - 0.5) * 0.4
+    grave.rotation.z = (Math.random() - 0.5) * 0.4
+    grave.rotation.x = (Math.random() - 0.5) * 0.3
+
+    graves.add(grave)
+}
+
 /**
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001).name('ambient intensity')
 scene.add(ambientLight)
 
 // Directional light
-const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
+const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.12)
 moonLight.position.set(4, 5, - 2)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001).name('moon intensity')
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
