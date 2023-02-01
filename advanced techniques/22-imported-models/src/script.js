5fc1a7e286f1bdf03e9a0f8fc80e67e024d1eee6
maxmay94 <maxmay94@gmail.com>
2023-02-01 17:35:03 -0500

    use DRACOLoader

--- advanced techniques/22-imported-models/src/script.js
+++ advanced techniques/22-imported-models/src/script.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import * as dat from 'lil-gui'
+
 
 /**
  * Base
@@ -13,6 +16,27 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+gltfLoader.load(
+    '/models/Duck/glTF-Draco/Duck.gltf',
+    // '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    (gltf) => {
+        scene.add(gltf.scene)
+        // const children = [...gltf.scene.children]
+        // for(const child of children) {
+        //     scene.add(child)
+        // }
+    }
+)
+
 
 /**
  * Floor
