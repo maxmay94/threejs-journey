39806e3a2ac4166c681ca53cac0b6eaa3a39268d
maxmay94 <maxmay94@gmail.com>
2023-02-07 10:20:07 -0500

    fix typo

--- advanced techniques/26-code-structuring-for-bigger-projects/src/Experience/Renderer.js
+++ advanced techniques/26-code-structuring-for-bigger-projects/src/Experience/Renderer.js
@@ -5,7 +5,7 @@ export default class Renderer {
   constructor() {
     this.experience = new Experience()
     this.canvas = this.experience.canvas
-    this.sizes = this.experience.resize
+    this.sizes = this.experience.sizes
     this.scene = this.experience.scene
     this.camera = this.experience.camera
 
