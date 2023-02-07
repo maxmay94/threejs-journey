664c8b7443ee7c8945cc9577a9bcfc694725100f
maxmay94 <maxmay94@gmail.com>
2023-02-07 11:32:06 -0500

    add fox to scene

---
+++ advanced techniques/26-code-structuring-for-bigger-projects/src/Experience/World/Fox.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three'
+import Experience from "../Experience.js"
+
+export default class Fox {
+  constructor() {
+    this.experience = new Experience()
+    this.scene = this.experience.scene
+    this.resources = this.experience.resources
+
+    // setup
+    this.resource = this.resources.items.foxModel
+
+    this.setModel()
+
+  }
+
+  setModel() {
+    this.model = this.resource.scene
+    this.model.scale.set(0.02, 0.02, 0.02)
+    this.scene.add(this.model)
+
+    this.model.traverse((child) => {
+      if(child instanceof THREE.Mesh) {
+        child.castShadow = true
+      }
+    })
+  }
+}
--- advanced techniques/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ advanced techniques/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three'
 import Experience from "../Experience.js"
 import Environment from './Environment.js'
 import Floor from './Floor.js'
+import Fox from './Fox.js'
 
 export default class World {
   constructor() {
@@ -9,18 +9,11 @@ export default class World {
     this.scene = this.experience.scene
     this.resources = this.experience.resources
     
-    // Test Mesh
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial()
-      )
-      this.scene.add(testMesh)
-
-      this.resources.on('ready', () => {
-        // Setup
-        this.floor = new Floor()
-        this.environment = new Environment()
-      })
-
-    }
+    this.resources.on('ready', () => {
+      // Setup
+      this.floor = new Floor()
+      this.fox = new Fox()
+      this.environment = new Environment()
+    })
+  }
 }
