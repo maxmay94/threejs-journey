16816bf6780f86f7914975856f069783384000a4
maxmay94 <maxmay94@gmail.com>
2023-02-22 11:47:47 -0500

    use perlin noise to make cool b + w portal effect

--- portal scene/39-adding-details-to-the-scene/src/script.js
+++ portal scene/39-adding-details-to-the-scene/src/script.js
@@ -55,6 +55,9 @@ const poleLightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffe5 })
 
 // Portal light material
 const portalLightMaterial = new THREE.ShaderMaterial({ 
+    uniforms: {
+        uTime: { value: 0 }
+    },
     vertexShader: portalVertexShader,
     fragmentShader: portalFragmentShader,
  })
@@ -191,6 +194,7 @@ const tick = () =>
 
     // update materials
     firefliesMaterial.uniforms.uTime.value = elapsedTime
+    portalLightMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
