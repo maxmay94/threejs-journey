a155465f82bff06df191d5ea476b053201d9fb11
maxmay94 <maxmay94@gmail.com>
2023-02-23 12:11:57 -0500

    leva debug tool

--- react three fiber/44-debug-a-r3f-application/src/Experience.jsx
+++ react three fiber/44-debug-a-r3f-application/src/Experience.jsx
@@ -1,28 +1,53 @@
 import { OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
 
-        <OrbitControls makeDefault />
+    const { position, colorSphere, visible } = useControls('sphere',{
+        position: {
+            value: {x: -2, y: 0},
+            min: -4,
+            max: 4,
+            step: 0.01,
+            joystick: 'invertY'
+        },
+        colorSphere: '#ffc900',
+        visible: true,
+    })
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+    const { scale, colorCube } = useControls('cube', {
+        scale:{
+            value: 1.5,
+            step: 0.05,
+            min: 0,
+            max: 5
+        },
+        colorCube: 'mediumpurple',
+    })
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+    return (
+        <>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <OrbitControls makeDefault />
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+            <ambientLight intensity={ 0.5 } />
 
-    </>
+            <mesh position={ [position.x, position.y, 0] } visible={visible} >
+                <sphereGeometry />
+                <meshStandardMaterial color={colorSphere} />
+            </mesh>
+
+            <mesh position-x={ 2 } scale={ scale }>
+                <boxGeometry />
+                <meshStandardMaterial color={colorCube} />
+            </mesh>
+
+            <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+                <planeGeometry />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
+
+        </>
+    )
 }
