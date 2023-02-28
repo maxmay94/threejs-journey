eafa7f19cb6185b5b14d6a6f378835c5b174e458
maxmay94 <maxmay94@gmail.com>
2023-02-28 12:45:40 -0500

    accumulative shadow

--- react three fiber/45-environment-and-staging-with-r3f/src/Experience.jsx
+++ react three fiber/45-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,40 +1,86 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { 
+    OrbitControls, 
+    useHelper, BakeShadows, 
+    softShadows, 
+    AccumulativeShadows, 
+    RandomizedLight 
+} from '@react-three/drei'
 import { useRef } from 'react'
 import { Perf } from 'r3f-perf'
+import * as THREE from 'three'
+
+// softShadows({
+//     frustum: 3.75, 
+//     size: 0.005,
+//     near: 9.5,
+//     samples: 17,
+//     rings: 11
+// })
 
 export default function Experience()
 {
+    const directionalLight = useRef()
+    useHelper(directionalLight, THREE.DirectionalLightHelper, 1)
+
     const cube = useRef()
+
     
-    useFrame((state, delta) =>
-    {
+    useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2
     })
 
-    return <>
+    return (
+        <>
+            <BakeShadows />
+            <color args={['ivory']} attach="background" />
 
-        <Perf position="top-left" />
+            <Perf position="top-left" />
 
-        <OrbitControls makeDefault />
+            <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+            <AccumulativeShadows
+                position={[0, -0.99, 0]}
+                scale={10}
+            >
+                <RandomizedLight 
+                    position={[1, 2, 3]} 
+                    amount={10}
+                    radius={3}
+                />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+            </AccumulativeShadows>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <directionalLight 
+                ref={directionalLight}
+                position={ [ 1, 2, 3 ] } 
+                intensity={ 1.5 } 
+                castShadow
+                shadow-mapSize={ [ 1024, 1024 ] } 
+                shadow-camera-near={ 1 }
+                shadow-camera-far={ 10 }
+                shadow-camera-top={ 5 }
+                shadow-camera-right={ 5 }
+                shadow-camera-bottom={ -5 }
+                shadow-camera-left={ -5 }
+            />
+            <ambientLight intensity={ 0.5 } />
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <mesh castShadow position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
 
-    </>
+            <mesh castShadow ref={ cube } position-x={ 2 } scale={ 1.5 }>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+
+            <mesh  position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+                <planeGeometry  />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
+
+        </>
+    )
 }
--- react three fiber/45-environment-and-staging-with-r3f/src/index.jsx
+++ react three fiber/45-environment-and-staging-with-r3f/src/index.jsx
@@ -1,18 +1,26 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
+// import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
+// const created = ({ gl, scene }) => {
+    // gl.setClearColor('#ff0000', 1)
+    // scene.background = new THREE.Color('pink')
+// }
+
 root.render(
     <Canvas
+        shadows
         camera={ {
             fov: 45,
             near: 0.1,
             far: 200,
             position: [ - 4, 3, 6 ]
         } }
+        // onCreated={ created }
     >
         <Experience />
     </Canvas>
