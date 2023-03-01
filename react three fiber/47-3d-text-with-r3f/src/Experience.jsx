0391a32199be4ad22b4c8c4513ac8d28c508a52c
maxmay94 <maxmay94@gmail.com>
2023-03-01 13:02:28 -0500

    optomize geometry and materials

--- react three fiber/47-3d-text-with-r3f/src/Experience.jsx
+++ react three fiber/47-3d-text-with-r3f/src/Experience.jsx
@@ -1,18 +1,60 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Text3D, Center, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useState } from 'react'
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+    const [torusGeometry, setTorusGeometry] = useState()
+    const [material, setMaterial] = useState()
 
-        <Perf position="top-left" />
+    const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
 
-        <OrbitControls makeDefault />
+    return (
+        <>
+            <Perf position="top-left" />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+            <OrbitControls makeDefault />
 
-    </>
+            <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+            <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
+
+            <Center>
+                <Text3D 
+                    font="/fonts/helvetiker_regular.typeface.json"
+                    size={0.75}
+                    height={0.2}
+                    curveSegments={12}
+                    bevelEnabled
+                    bevelThickness={0.02}
+                    bevelSize={0.02}
+                    bevelOffset={0}
+                    bevelSegments={5}
+                    material={material}
+                > 
+                    HELLO R3F
+                </Text3D>
+            </Center>
+
+            {
+                [...Array(100)].map((_, index) => (
+                    <mesh 
+                        geometry={torusGeometry}
+                        material={material}
+                        key={index} 
+                        position={[
+                            (Math.random() - 0.5) * 10,
+                            (Math.random() - 0.5) * 10, 
+                            (Math.random() - 0.5) * 10
+                        ] } 
+                        scale={0.2 + Math.random() * 0.2}
+                        rotation={[
+                            Math.random() * Math.PI,
+                            Math.random() * Math.PI,
+                            0
+                        ]}
+                   />
+                ))
+            }
+
+        </>
+    )
 }
