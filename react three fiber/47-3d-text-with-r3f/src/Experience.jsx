209132a55dec32f49f94f24c72b6cb87c4fd0bf8
maxmay94 <maxmay94@gmail.com>
2023-03-01 13:22:35 -0500

    lesson complete

--- react three fiber/47-3d-text-with-r3f/src/Experience.jsx
+++ react three fiber/47-3d-text-with-r3f/src/Experience.jsx
@@ -1,21 +1,37 @@
 import { OrbitControls, Text3D, Center, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useState } from 'react'
+import { useEffect, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience() {
-    const [torusGeometry, setTorusGeometry] = useState()
-    const [material, setMaterial] = useState()
+
+    const donuts = useRef([])
 
     const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+
+    useEffect(() => {
+        matcapTexture.encoding = THREE.sRGBEncoding
+        matcapTexture.needsUpdate = true
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
+    useFrame((state, delta) => {
+        for(const donut of donuts.current) {
+            donut.rotation.x += 0.1 * delta
+            donut.rotation.y += 0.2 * delta
+        }
+    })
 
     return (
         <>
             <Perf position="top-left" />
 
             <OrbitControls makeDefault />
-
-            <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
-            <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
 
             <Center>
                 <Text3D 
@@ -35,8 +51,9 @@ export default function Experience() {
             </Center>
 
             {
-                [...Array(100)].map((_, index) => (
+                [...Array(100)].map((donut, index) => (
                     <mesh 
+                        ref = { (element) => donuts.current[index] = element }
                         geometry={torusGeometry}
                         material={material}
                         key={index} 
@@ -51,9 +68,10 @@ export default function Experience() {
                             Math.random() * Math.PI,
                             0
                         ]}
-                   />
+                />
                 ))
             }
+
 
         </>
     )
