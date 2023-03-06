4ec6b987ac4eabe96f11f0b5aeb140e956aac6d2
maxmay94 <maxmay94@gmail.com>
2023-03-06 12:12:42 -0500

    change cube color on click

--- react three fiber/49-mouse-events-with-r3f/src/Experience.jsx
+++ react three fiber/49-mouse-events-with-r3f/src/Experience.jsx
@@ -11,27 +11,33 @@ export default function Experience()
         cube.current.rotation.y += delta * 0.2
     })
 
-    return <>
+    const eventHandler = () => {
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
+    }
 
-        <OrbitControls makeDefault />
+    return (
+        <>
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+            <OrbitControls makeDefault />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+            <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+            <ambientLight intensity={ 0.5 } />
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <mesh position-x={ - 2 }  >
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <mesh ref={ cube } position-x={ 2 } scale={ 1.5 } onClick={eventHandler} >
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
 
-    </>
+            <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+                <planeGeometry />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
+
+        </>
+    )
 }
