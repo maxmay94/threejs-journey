63264c025696aaf931183d10e3fbbd140b76bde4
maxmay94 <maxmay94@gmail.com>
2023-03-06 12:42:49 -0500

    add burger

--- react three fiber/49-mouse-events-with-r3f/src/Experience.jsx
+++ react three fiber/49-mouse-events-with-r3f/src/Experience.jsx
@@ -1,17 +1,19 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 import { useRef } from 'react'
 
 export default function Experience()
 {
     const cube = useRef()
+
+    const hamburger = useGLTF('./hamburger.glb')
     
     useFrame((state, delta) =>
     {
         cube.current.rotation.y += delta * 0.2
     })
 
-    const eventHandler = () => {
+    const eventHandler = (event) => {
         cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
     }
 
@@ -23,12 +25,19 @@ export default function Experience()
             <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
             <ambientLight intensity={ 0.5 } />
 
-            <mesh position-x={ - 2 }  >
+            <mesh position-x={ - 2 } onClick={ (event) => event.stopPropagation() }  >
                 <sphereGeometry />
                 <meshStandardMaterial color="orange" />
             </mesh>
 
-            <mesh ref={ cube } position-x={ 2 } scale={ 1.5 } onClick={eventHandler} >
+            <mesh 
+                ref={ cube } 
+                position-x={ 2 } 
+                scale={ 1.5 } 
+                onClick={eventHandler}  
+                onPointerEnter={() => document.body.style.cursor = 'pointer' }
+                onPointerLeave={() => document.body.style.cursor = 'default' }
+            >
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
             </mesh>
@@ -37,6 +46,16 @@ export default function Experience()
                 <planeGeometry />
                 <meshStandardMaterial color="greenyellow" />
             </mesh>
+
+            <primitive 
+                object={ hamburger.scene } 
+                position={ [ 0, 1, 0 ] } 
+                scale={ 0.25 } 
+                onClick={(event) => {
+                    event.stopPropagation()
+                    console.log('clicked hamburger')
+                }}
+            />
 
         </>
     )
