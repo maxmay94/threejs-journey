5412b8f8e8d562aa7b4365fcba7804a77af2aa8f
maxmay94 <maxmay94@gmail.com>
2023-03-10 12:07:56 -0500

    add impulse

--- react three fiber/52-physics-with-r3f/src/Experience.jsx
+++ react three fiber/52-physics-with-r3f/src/Experience.jsx
@@ -1,31 +1,64 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { Physics, RigidBody, Debug, CuboidCollider, BallCollider } from '@react-three/rapier'
+import { useRef } from 'react'
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+    const cube = useRef()
 
-        <Perf position="top-left" />
+    const cubeJump = (event) => {
+        cube.current.applyImpulse({x: 0, y: 5, z: 0})
+        cube.current.applyTorqueImpulse({x: Math.random() - 0.5, y: Math.random() - 0.5, z: Math.random() - 0.5}, event.point)
+    }
 
-        <OrbitControls makeDefault />
+    return (
+        <>
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+            <Perf position="top-left" />
 
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+            <OrbitControls makeDefault />
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+            <ambientLight intensity={ 0.5 } />
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <Physics gravity={[0, -9.82, 0 ]}>   
 
-    </>
+                <Debug />
+
+                <RigidBody colliders="ball" restitution={ 0.5 } >          
+                    <mesh castShadow position={ [ -1.5, 2, 0 ] }>
+                        <sphereGeometry />
+                        <meshStandardMaterial color="orange" />
+                    </mesh>
+                </RigidBody>
+
+
+                <RigidBody 
+                    ref={cube} 
+                    position={[1.5, 2, 0]} 
+                    gravityScale={ 1 } 
+                    restitution={ 0 } 
+                    friction={ 0.7 }
+                    mass={ 1 }
+                >
+                    <mesh castShadow  onClick={ cubeJump } >
+                        <boxGeometry />
+                        <meshStandardMaterial color="mediumpurple" />
+                    </mesh>
+                </RigidBody>
+
+
+                <RigidBody 
+                    type='fixed' 
+                    friction={ 0.7 } 
+                >
+                    <mesh receiveShadow position-y={ - 1.25 }>
+                        <boxGeometry args={ [ 10, 0.5, 10 ] } />
+                        <meshStandardMaterial color="greenyellow" />
+                    </mesh>
+                </RigidBody>
+            </Physics>
+
+        </>
+    )
 }
