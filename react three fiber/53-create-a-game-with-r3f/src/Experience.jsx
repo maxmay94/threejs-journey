19a4eb2698a6a154c9a6c8a94e184bfd6fe4529b
maxmay94 <maxmay94@gmail.com>
2023-03-14 15:48:04 -0400

    fix bug causing Vite HMR to stop

--- react three fiber/53-create-a-game-with-r3f/src/Experience.jsx
+++ react three fiber/53-create-a-game-with-r3f/src/Experience.jsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from '@react-three/drei'
 import { Physics, Debug } from '@react-three/rapier'
 import Lights from './Lights.jsx'
-import Level from './level.jsx'
+import Level from './Level.jsx'
 
 export default function Experience() {
     return (
--- react three fiber/53-create-a-game-with-r3f/src/Level.jsx
+++ react three fiber/53-create-a-game-with-r3f/src/Level.jsx
@@ -53,6 +53,33 @@ function BlockSpinner({position = [0, 0, 0]}) {
   )
 }
 
+function BlockLimbo({position = [0, 0, 0]}) {
+  const [speed] = useState(() => (Math.random() + 0.02) * (Math.random() > 0.5 ? 1 : -1))
+  const obstacle = useRef()
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    const rotation = new THREE.Quaternion()
+    rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+    obstacle.current.setNextKinematicRotation(rotation)
+  })
+
+    return (
+      <group position={ position }>
+        <mesh 
+          geometry={boxGeometry} 
+          material={floor2Material} 
+          position={[0, -0.1, 0]} 
+          scale={[4, 0.2, 4]} 
+          receiveShadow 
+        />
+        <RigidBody ref={obstacle} type='kinematicTranslation' position={[ 0, 0.3, 0 ]} restitution={0.2} friction={0} >
+          <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} castShadow receiveShadow/>
+        </RigidBody>
+      </group>
+  )
+}
+
 export default function Level() {
 
 
@@ -60,6 +87,7 @@ export default function Level() {
     <>
       <BlockStart position={[0, 0, 4]} />
       <BlockSpinner position={[0, 0, 0]} />
+      <BlockLimbo position={[0, 0, -4]} />
     </>
   )
 }
