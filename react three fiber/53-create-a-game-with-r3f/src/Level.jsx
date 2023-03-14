e5e17f21d819449b6ad068708c5fe2626f251e0e
maxmay94 <maxmay94@gmail.com>
2023-03-14 15:16:36 -0400

    spin trap done

--- react three fiber/53-create-a-game-with-r3f/src/Level.jsx
+++ react three fiber/53-create-a-game-with-r3f/src/Level.jsx
@@ -1,9 +1,55 @@
-function BlockStart() {
+import * as THREE from 'three'
+import { RigidBody } from '@react-three/rapier'
+import { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+THREE.ColorManagement.legacyMode = false
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+
+const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' })
+const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: 'orangered' })
+const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' })
+
+function BlockStart({position = [0, 0, 0]}) {
     return (
-    <mesh position={[0, -0.1, 0]} receiveShadow>
-        <boxGeometry args={ [ 4, 0.2, 4 ] } />
-        <meshStandardMaterial color="limegreen" />
-    </mesh>
+      <group position={ position }>
+        <mesh 
+          geometry={boxGeometry} 
+          material={floor1Material} 
+          position={[0, -0.1, 0]} 
+          scale={[4, 0.2, 4]} 
+          receiveShadow 
+        />
+      </group>
+  )
+}
+
+function BlockSpinner({position = [0, 0, 0]}) {
+  const [speed] = useState(() => (Math.random() + 0.02) * (Math.random() > 0.5 ? 1 : -1))
+  const obstacle = useRef()
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    const rotation = new THREE.Quaternion()
+    rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+    obstacle.current.setNextKinematicRotation(rotation)
+  })
+
+    return (
+      <group position={ position }>
+        <mesh 
+          geometry={boxGeometry} 
+          material={floor2Material} 
+          position={[0, -0.1, 0]} 
+          scale={[4, 0.2, 4]} 
+          receiveShadow 
+        />
+        <RigidBody ref={obstacle} type='kinematicPosition' position={[ 0, 0.3, 0 ]} restitution={0.2} friction={0} >
+          <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} castShadow receiveShadow/>
+        </RigidBody>
+      </group>
   )
 }
 
@@ -12,7 +58,8 @@ export default function Level() {
 
   return (
     <>
-      <BlockStart />
+      <BlockStart position={[0, 0, 4]} />
+      <BlockSpinner position={[0, 0, 0]} />
     </>
   )
 }
