4a82ca105ad1247a548408909fbf11521bc1959c
maxmay94 <maxmay94@gmail.com>
2023-03-14 16:43:07 -0400

    add player ball

--- react three fiber/53-create-a-game-with-r3f/src/Experience.jsx
+++ react three fiber/53-create-a-game-with-r3f/src/Experience.jsx
@@ -1,7 +1,8 @@
 import { OrbitControls } from '@react-three/drei'
 import { Physics, Debug } from '@react-three/rapier'
-import Lights from './Lights.jsx'
 import { Level } from './Level.jsx'
+import Lights from './Lights.jsx'
+import Player from './Player.jsx'
 
 export default function Experience() {
     return (
@@ -12,6 +13,7 @@ export default function Experience() {
                 <Debug />
                 <Lights />
                 <Level />
+                <Player />
             </Physics>
 
 
---
+++ react three fiber/53-create-a-game-with-r3f/src/Player.jsx
@@ -0,0 +1,15 @@
+import { RigidBody } from "@react-three/rapier"
+
+export default function Player() {
+
+  return(
+    <>
+      <RigidBody colliders="ball" position={[0, 1, 0]} restitution={0.2} friction={1} >
+        <mesh castShadow>
+          <icosahedronGeometry args={[0.3, 1]} />
+          <meshStandardMaterial flatShading color="hotpink" />
+        </mesh>
+      </RigidBody>
+    </>
+  )
+}
