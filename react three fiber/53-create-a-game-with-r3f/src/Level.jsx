203dd8508dcfeffe9f0a537b645f04310eb782a0
maxmay94 <maxmay94@gmail.com>
2023-03-14 16:12:10 -0400

    add burger to scene

--- react three fiber/53-create-a-game-with-r3f/src/Level.jsx
+++ react three fiber/53-create-a-game-with-r3f/src/Level.jsx
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { RigidBody } from '@react-three/rapier'
 import { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
 
 THREE.ColorManagement.legacyMode = false
 
@@ -22,6 +23,29 @@ function BlockStart({position = [0, 0, 0]}) {
           scale={[4, 0.2, 4]} 
           receiveShadow 
         />
+      </group>
+  )
+}
+
+function BlockEnd({position = [0, 0, 0]}) {
+  const hamburger = useGLTF('./hamburger.glb')
+
+  hamburger.scene.children.forEach((mesh) => {
+    mesh.castShadow = true
+  })
+
+    return (
+      <group position={ position }>
+        <mesh 
+          geometry={boxGeometry} 
+          material={floor1Material} 
+          position={[0, -0 , 0]} 
+          scale={[4, 0.2, 4]} 
+          receiveShadow 
+        />
+        <RigidBody type="fixed" colliders="hull" position={[0, 0.25, 0]} restitution={0.2} friction={0} >
+          <primitive  object={hamburger.scene} scale={0.2} />
+        </RigidBody>
       </group>
   )
 }
@@ -54,14 +78,14 @@ function BlockSpinner({position = [0, 0, 0]}) {
 }
 
 function BlockLimbo({position = [0, 0, 0]}) {
-  const [speed] = useState(() => (Math.random() + 0.02) * (Math.random() > 0.5 ? 1 : -1))
+  const [timeOffset] = useState(() => Math.random() * Math.PI * 2)
   const obstacle = useRef()
 
-  useFrame((state) => {
-    const time = state.clock.getElapsedTime()
-    const rotation = new THREE.Quaternion()
-    rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
-    obstacle.current.setNextKinematicRotation(rotation)
+  useFrame((state) =>{
+      const time = state.clock.getElapsedTime()
+  
+      const y = Math.sin(time + timeOffset) + 1.15
+      obstacle.current.setNextKinematicTranslation({ x: position[0], y: position[1] + y, z: position[2] })
   })
 
     return (
@@ -73,21 +97,48 @@ function BlockLimbo({position = [0, 0, 0]}) {
           scale={[4, 0.2, 4]} 
           receiveShadow 
         />
-        <RigidBody ref={obstacle} type='kinematicTranslation' position={[ 0, 0.3, 0 ]} restitution={0.2} friction={0} >
+        <RigidBody ref={obstacle} type='kinematicPosition' position={[ 0, 0.3, 0 ]} restitution={0.2} friction={0} >
           <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} castShadow receiveShadow/>
         </RigidBody>
       </group>
   )
 }
 
+function BlockAxe({position = [0, 0, 0]}) {
+  const [timeOffset] = useState(() => Math.random() * Math.PI * 2)
+  const obstacle = useRef()
+
+  useFrame((state) =>{
+      const time = state.clock.getElapsedTime()
+  
+      const x = Math.sin(time + timeOffset) * 1.25
+      obstacle.current.setNextKinematicTranslation({ x: position[0] + x, y: position[1] + 0.75, z: position[2] })
+  })
+
+    return (
+      <group position={ position }>
+        <mesh 
+          geometry={boxGeometry} 
+          material={floor2Material} 
+          position={[0, -0.1, 0]} 
+          scale={[4, 0.2, 4]} 
+          receiveShadow 
+        />
+        <RigidBody ref={obstacle} type='kinematicPosition' position={[ 0, 0.3, 0 ]} restitution={0.2} friction={0} >
+          <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[1.5, 1.5, 0.3]} castShadow receiveShadow/>
+        </RigidBody>
+      </group>
+  )
+}
+
 export default function Level() {
-
-
   return (
     <>
-      <BlockStart position={[0, 0, 4]} />
-      <BlockSpinner position={[0, 0, 0]} />
-      <BlockLimbo position={[0, 0, -4]} />
+      <BlockStart position={[0, 0, 16]} />
+      <BlockSpinner position={[0, 0, 12]} />
+      <BlockLimbo position={[0, 0, 8]} />
+      <BlockAxe position={[0, 0, 4]} />
+      <BlockEnd position={[0, 0, 0]} />
     </>
   )
 }
